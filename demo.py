38d05556297be6b116a36c433c11801353e23935
anant15 <anant14015@iiitd.ac.in>
2020-06-08 11:36:25 +0530

    Add hand detectio and head detection code

--- demo.py
+++ demo.py
@@ -10,6 +10,7 @@ from modules.load_state import load_state
 from modules.pose import Pose, track_poses
 from val import normalize, pad_width
 
+import time
 
 class ImageReader(object):
     def __init__(self, file_names):
@@ -54,9 +55,11 @@ class VideoReader(object):
 def infer_fast(net, img, net_input_height_size, stride, upsample_ratio, cpu,
                pad_value=(0, 0, 0), img_mean=(128, 128, 128), img_scale=1/256):
     height, width, _ = img.shape
+    print(height, net_input_height_size, net_input_height_size / height)
     scale = net_input_height_size / height
 
     scaled_img = cv2.resize(img, (0, 0), fx=scale, fy=scale, interpolation=cv2.INTER_CUBIC)
+    print(scaled_img.shape)
     scaled_img = normalize(scaled_img, img_mean, img_scale)
     min_dims = [net_input_height_size, max(scaled_img.shape[1], net_input_height_size)]
     padded_img, pad = pad_width(scaled_img, stride, pad_value, min_dims)
@@ -65,7 +68,10 @@ def infer_fast(net, img, net_input_height_size, stride, upsample_ratio, cpu,
     if not cpu:
         tensor_img = tensor_img.cuda()
 
+    start = time.time()
     stages_output = net(tensor_img)
+    end = time.time()
+    print("Time taken", end-start)
 
     stage2_heatmaps = stages_output[-2]
     heatmaps = np.transpose(stage2_heatmaps.squeeze().cpu().data.numpy(), (1, 2, 0))
@@ -78,11 +84,15 @@ def infer_fast(net, img, net_input_height_size, stride, upsample_ratio, cpu,
     return heatmaps, pafs, scale, pad
 
 
-def run_demo(net, image_provider, height_size, cpu, track, smooth):
+def run_demo(net, image_provider, height_size, cpu, track, smooth, output_path):
     net = net.eval()
     if not cpu:
         net = net.cuda()
 
+    output_path = output_path.split(".")[0] + "-annotations" + ".mp4"
+    print(output_path)
+    #(1080, 1920, 3)
+    out = cv2.VideoWriter(output_path,cv2.VideoWriter_fourcc('m','p','4','v'), 10, (1920,1080))
     stride = 8
     upsample_ratio = 4
     num_keypoints = Pose.num_kpts
@@ -90,6 +100,8 @@ def run_demo(net, image_provider, height_size, cpu, track, smooth):
     delay = 33
     for img in image_provider:
         orig_img = img.copy()
+        print(orig_img.shape)
+        #print(output_path)
         heatmaps, pafs, scale, pad = infer_fast(net, img, height_size, stride, upsample_ratio, cpu)
 
         total_keypoints_num = 0
@@ -118,7 +130,7 @@ def run_demo(net, image_provider, height_size, cpu, track, smooth):
             previous_poses = current_poses
         for pose in current_poses:
             pose.draw(img)
-        img = cv2.addWeighted(orig_img, 0.6, img, 0.4, 0)
+        img = cv2.addWeighted(orig_img, 0.3, img, 0.7, 0)
         for pose in current_poses:
             cv2.rectangle(img, (pose.bbox[0], pose.bbox[1]),
                           (pose.bbox[0] + pose.bbox[2], pose.bbox[1] + pose.bbox[3]), (0, 255, 0))
@@ -126,6 +138,7 @@ def run_demo(net, image_provider, height_size, cpu, track, smooth):
                 cv2.putText(img, 'id: {}'.format(pose.id), (pose.bbox[0], pose.bbox[1] - 16),
                             cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 255))
         cv2.imshow('Lightweight Human Pose Estimation Python Demo', img)
+        out.write(img)
         key = cv2.waitKey(delay)
         if key == 27:  # esc
             return
@@ -134,6 +147,7 @@ def run_demo(net, image_provider, height_size, cpu, track, smooth):
                 delay = 0
             else:
                 delay = 33
+    out.release()
 
 
 if __name__ == '__main__':
@@ -163,4 +177,4 @@ if __name__ == '__main__':
     else:
         args.track = 0
 
-    run_demo(net, frame_provider, args.height_size, args.cpu, args.track, args.smooth)
+    run_demo(net, frame_provider, args.height_size, args.cpu, args.track, args.smooth, args.video)
